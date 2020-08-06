d72bb71ebf37b91873cec1740ef61893d987310b
speedstershubham <shubhamchowksay10@gmail.com>
2020-08-06 16:40:37 +0530

    first commit

---
+++ build/precache-manifest.f1e50e29b64b3a3e995b50bb3751f115.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f570e040eee027772890590227846f54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85714ad9459c1aa6adfa",
+    "url": "/static/css/main.5d41ca5f.chunk.css"
+  },
+  {
+    "revision": "16ae329a132f67f752d9",
+    "url": "/static/js/2.bfb0693a.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.bfb0693a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85714ad9459c1aa6adfa",
+    "url": "/static/js/main.a8f66cae.chunk.js"
+  },
+  {
+    "revision": "3339d60833c4b1f02a1a",
+    "url": "/static/js/runtime-main.170c15dd.js"
+  }
+]);
